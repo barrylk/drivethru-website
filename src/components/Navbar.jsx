e0fb5cd59cb4d2323ea03400e34e296df04f2bf0
barrylk <114392920+barrylk@users.noreply.github.com>
2025-04-24 12:42:38 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>DriveThru</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/rider-login">Rider Login</Link></li>
+        <li><Link to="/locate-me">Locate Me</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
