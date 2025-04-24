4123b5482545c8808032aca01615b2adb812e61c
barrylk <114392920+barrylk@users.noreply.github.com>
2025-04-24 12:45:32 +0530

    Update LocateMe.jsx

--- src/pages/LocateMe.jsx
+++ src/pages/LocateMe.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const LocateMe = () => {
+  return (
+    <div>
+      <h2>Locate Me</h2>
+      <p>This feature is under construction.</p>
+    </div>
+  );
+};
+
+export default LocateMe;
