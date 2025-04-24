92d28bba50e2558c484ae409efe1c46ee5551086
barrylk <114392920+barrylk@users.noreply.github.com>
2025-04-24 11:58:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import RiderLogin from "./pages/RiderLogin";
-import Services from "./pages/Services";
-import Map from "./pages/Map";
-import CartChat from "./pages/CartChat";
-import Feedback from "./pages/Feedback";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import RiderLogin from './pages/RiderLogin';
+import LocateMe from './pages/LocateMe';
+import './index.css';
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/rider-login" element={<RiderLogin />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/cart-chat" element={<CartChat />} />
-        <Route path="/feedback" element={<Feedback />} />
-      </Routes>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/rider-login" element={<RiderLogin />} />
+          <Route path="/locate-me" element={<LocateMe />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
+
+export default App;
